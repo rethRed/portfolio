3c03d527253c927ab1fd2682c6c1810ac0c808af
Daniel-D3V <personal.danielsilva.d3v@outlook.com>
2023-09-06 22:46:21 -0300

    ve

--- src/app/projects/@root/Molecules/ProjectCard/index.tsx
+++ src/app/projects/@root/Molecules/ProjectCard/index.tsx
@@ -12,8 +12,8 @@ export const ProjectCard = (props: ProjectCardProps) => {
         <div className="bg-secondary rounded-lg">
             <Image className="rounded-lg rounded-b-none" src={props.image} width={1080} height={1080} alt={props.title} />
             <div className="p-4">
-                <p className="text-base font-semibold font-sans">{props.title}</p>
-                <p className="font-sans text-xs text-gray-400 font-semibold">{props.description}</p>
+                <p className="text-base font-semibold font-sans mb-2">{props.title}</p>
+                <p className="font-sans text-xs text-gray-400 font-medium">{props.description}</p>
                 <p className="mt-3 flex items-center justify-start gap-2">
                     {props.languages.map((language, index) => (
                         <span className="text-xs p-1 px-2 bg-indigo-900 rounded-lg text-indigo-300 font-bold font-mono" key={index}>{language}</span>
