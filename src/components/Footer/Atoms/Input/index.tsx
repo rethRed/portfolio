c00ec53b45308fdbaa9dbeb082a2ea445930d34b
Daniel-D3V <personal.danielsilva.d3v@outlook.com>
2023-09-07 05:04:20 -0300

    save

--- src/components/Footer/Atoms/Input/index.tsx
+++ src/components/Footer/Atoms/Input/index.tsx
@@ -4,6 +4,6 @@ type Props = HTMLAttributes<HTMLInputElement>
 
 export const Input = (props: Props) => {
     return (
-        <input {...props} className="bg-secondary text-gray-400 max-w-full w-full px-4 py-4 rounded-lg border border-transparent focus:border-blue-500 outline-none transition" />
+        <input {...props} className="bg-secondary text-gray-400 max-w-full w-full px-4 py-4 rounded-lg border border-transparent focus:border-indigo-500 outline-none transition" />
     )
 }
--- src/components/Footer/Atoms/Textarea/index.tsx
+++ src/components/Footer/Atoms/Textarea/index.tsx
@@ -4,6 +4,6 @@ type Props = HTMLAttributes<HTMLTextAreaElement>
 
 export const Textarea = (props: Props) => {
     return (
-        <textarea {...props} className="bg-secondary text-gray-400 max-w-full w-full px-4 py-4 rounded-lg border border-transparent focus:border-blue-500 outline-none transition" />
+        <textarea {...props} className="bg-secondary text-gray-400 max-w-full w-full px-4 py-4 rounded-lg border border-transparent focus:border-indigo-500 outline-none transition" />
     )
 }
