4ef2efc3a550eedb4d6df26f3305f2eb204330cc
rethred <gabrielwastha@gmail.com>
2023-09-14 19:03:14 -0300

    save

--- src/app/home/@root/sections/Organizations/Molecules/Details/index.tsx
+++ src/app/home/@root/sections/Organizations/Molecules/Details/index.tsx
@@ -4,9 +4,9 @@ export const Details = () => {
             <p className="text-mono text-sm font-semibold text-indigo-400 mb-3">./organizations</p>
             <p className="font-semibold text-2xl mb-1">My Experiences in Organizations</p>
             <p className="text-sm font-medium text-gray-400">
-                As a pivotal part of the talented team at Warp Store, I'm fully immersed 
+                As a pivotal part of the talented team at Warp Store, I&apos;m fully immersed 
                 in the realm of modern development. Leveraging Docker, clean architecture, and DDD principles, 
-                I'm crafting solutions that stand the test of time. My toolkit includes Node.js and TypeScript, 
+                I&apos;m crafting solutions that stand the test of time. My toolkit includes Node.js and TypeScript, 
                 and I ensure the security of the APIs I develop, prioritizing the protection of user data and 
                 system integrity.
             </p>
